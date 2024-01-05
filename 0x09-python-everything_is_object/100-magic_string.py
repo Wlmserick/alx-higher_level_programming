34ab103311110b510c0ad4a7577f0831cd291831
Wlmserick <erickwlms@gmail.com>
2024-01-05 13:01:50 -0800

    python objects

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def magic_string():
-    return "".join(["BestSchool" + str(i) for i in range(1, n + 1)])
-
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
